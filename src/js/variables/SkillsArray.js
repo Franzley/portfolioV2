13bce90de45037c0d951e55ace48273f00402ea2
Franzley <fbonhomme9721@gmail.com>
2022-09-21 11:21:08 -0400

    Removed Duplicate Skill

--- src/js/variables/SkillsArray.js
+++ src/js/variables/SkillsArray.js
@@ -1,4 +1,4 @@
-export const skillsLanguage = ["HTML", "CSS", "Java", "JavaScript", "Java"];
+export const skillsLanguage = ["HTML", "CSS", "JavaScript", "Java"];
 export const skillsFramework = ["Bootstrap", "Material UI"];
 export const skillsTools = ["React", "GIT", "Firebase", "Firestore", "Rapid API"];
 export const skillsOthers = ["Photoshop"];
